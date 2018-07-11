d2fa669be0bbeb99bdef9ccfa381862163747a02
Suhirtha Raj <suhirtha@fb.com>
2018-07-10 17:59:39 -0700

    Camera works, can't post

--- app/src/main/java/com/example/suhirtha/parsetagram/HomeActivity.java
+++ app/src/main/java/com/example/suhirtha/parsetagram/HomeActivity.java
@@ -1,13 +1,21 @@
 package com.example.suhirtha.parsetagram;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.suhirtha.parsetagram.models.Post;
@@ -16,7 +24,12 @@ import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
+import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
@@ -25,6 +38,15 @@ public class HomeActivity extends AppCompatActivity {
     private EditText mCaption;
     private Button mCreate;
     private Button mRefresh;
+
+    private ImageView mCapture;
+
+    private Uri file;
+
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private static final String imagePath = "/storage/emulated/0/DCIM/Camera/IMG_20180710_153941.jpg";
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -51,11 +73,44 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        mCaption = findViewById(R.id.etCaption); //TODO
+        mCaption = findViewById(R.id.etCaption);
         mCreate = findViewById(R.id.btnCreate);
         mRefresh = findViewById(R.id.btnRefresh);
+        mCapture = findViewById(R.id.ivCapture);
 
-        loadTopPosts();
+        /*
+        mCreate.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                final String caption = mCaption.getText().toString();
+                final ParseUser user = ParseUser.getCurrentUser();
+
+                final File file = new File(imagePath);
+                final ParseFile parseFile = new ParseFile(file);
+
+                createPost(caption, parseFile, user);
+            }
+        });
+        */
+        mRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadTopPosts();
+            }
+        });
+
+        if(Build.VERSION.SDK_INT>=24){
+            try{
+                Method m = StrictMode.class.getMethod("disableDeathOnFileUriExposure");
+                m.invoke(null);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+
     }
 
     private void loadTopPosts() {
@@ -96,8 +151,93 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
-    private void createPost(String description, ParseFile imageFile, ParseUser user) {
+    private void createPost(String caption, ParseFile imageFile, ParseUser user) {
+        final Post newPost = new Post();
+        newPost.setCaption(caption);
+        newPost.setImage(imageFile);
+        newPost.setUser(user);
 
+        newPost.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("HomeActivity.java", "Post successful.!");
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
+
+    /**
+     * FROM ANDROID DOCUMENTATION
+     */
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    /**
+     * FROM OTHER SHADY WEBSITE
+     * --------------------------------------------------------------------
+     * https://androidkennel.org/android-camera-access-tutorial/
+     */
+
+    /**
+     *
+     * @param view
+     */
+    public void takePicture(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        file = Uri.fromFile(getOutputMediaFile()); //TODO
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+
+        // we call startActivityForResult instead of just startActivity because we need a result
+        // to be returned
+        startActivityForResult(takePictureIntent, 100); //100 is some integer constant
+
+        //creating Parse file
+        final ParseFile parseFile = new ParseFile()
+    }
+
+    /**
+     * Now that we've sent the intent to the system, we have to handle the result.
+     * @param requestCode - hard-coded integer constant? TODO - what are all these random integers
+     * @param resultCode - another random integer constant?
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                mCapture.setImageURI(file);
+
+                /*
+                Bitmap takenImage = BitmapFactory.decodeFile(file.getPath());
+                mCapture.setImag;
+                 **/
+
+            }
+        }
+    }
+
+    private static File getOutputMediaFile() {
+        File mediaStorageDir = new File(Environment.
+                getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "ParsetagramCam");
+
+        if (mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("CameraDemo", "failed to create directory");
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+    }
+
 
 }
